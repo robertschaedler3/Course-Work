b89adfefc06186069d7d550bf9962c2db3f6eb53
Robert Schaedler III <robertschaedler3@gmail.com>
2019-11-09 11:37:44 -0500

    added functions

--- CS 284 (Data Structures)/notes/lists/SLL.java
+++ CS 284 (Data Structures)/notes/lists/SLL.java
@@ -1,6 +1,9 @@
 package notes.lists;
 
-import notes.pair.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import notes.pair.Pair;
 
 public class SLL<E> {
 
@@ -24,7 +27,7 @@ public class SLL<E> {
     private Node<E> head;
     private int size;
 
-    SLL() {
+    public SLL() {
         head = null;
         size = 0;
     }
@@ -81,10 +84,36 @@ public class SLL<E> {
             current = current.next;
         }
 
-        Node<E> temp = current;
+        E temp = current.data;
         current.next = null;
         size--;
-        return temp.data;
+        return temp;
+    }
+
+    public void append(E item) {
+        if (isEmpty()) {
+            head = new Node<E>(item);
+        } else {
+            Node<E> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = new Node<E>(item);
+        }
+        size++;
+    }
+
+    public void append(SLL<E> list) {
+        if (isEmpty()) {
+            head = list.head;
+        } else {
+            Node<E> current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = list.head;
+        }
+        size++;
     }
 
     private void addAfter(Node<E> n, E item) {
@@ -132,6 +161,7 @@ public class SLL<E> {
         SLL<E> result = new SLL<>();
         result.size = size;
         Node<E> current = head;
+
         Node<E> last = new Node<E>(null);
         Node<E> dummy = last;
 
@@ -155,9 +185,11 @@ public class SLL<E> {
             size = 0;
             return;
         }
-        // n>0 && n<size
-        for (int i = 0; i < n; i++) {
+        // n > 0 && n < size
+        int i = 0;
+        while (i < n) {
             removeFirst();
+            i++;
         }
     }
 
@@ -168,12 +200,12 @@ public class SLL<E> {
         if (n <= 0) { // return an empty list
             return new SLL<E>();
         }
-        // n>0 and n<size
-        // Look at the code for clone
+        // n > 0 and n < size
         SLL<E> result = new SLL<E>();
         Node<E> last = new Node<E>(null);
         Node<E> dummy = last;
         Node<E> current = head;
+
         for (int i = 0; i < n; i++) {
             last.next = new Node<E>(current.data);
             last = last.next;
@@ -206,6 +238,75 @@ public class SLL<E> {
         }
     }
 
+    public void reverse() {
+        if (head == null) { // empty list
+            throw new IllegalStateException();
+        }
+
+        if (head.next == null) { // singleton list
+            return;
+        }
+
+        Node<E> last = head;
+        Node<E> current = head.next;
+        last.next = null;
+        while (current != null) {
+            Node<E> temp = current.next;
+            current.next = last;
+            last = current;
+            current = temp;
+        }
+
+        head = last;
+    }
+
+    public boolean containsCycle() {
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        Node<E> fast = head;
+        Node<E> slow = head.next;
+        while (fast != slow && fast != null) {
+            fast = fast.next != null ? fast.next.next : null;
+            slow = slow.next;
+        }
+        return fast == slow;
+    }
+
+    public boolean containsCycleBad() {
+        if (head == null || head.next == null)
+            return false;
+
+        HashMap<Node<E>, Boolean> map = new HashMap<>();
+        Node<E> current = head;
+        while (!map.containsKey(current) && current != null) {
+            map.put(current, true);
+            current = current.next;
+        }
+        return current != null;
+    }
+
+    public void printReverse() {
+        Node<E> current = head;
+
+        System.out.print("[");
+        for (int i = 0; i < this.size(); i++) {
+            for (int j = 0; j < this.size() - 1 - i; j++) {
+                current = current.next;
+            }
+            System.out.print(current.data + ",");
+            current = head;
+        }
+        System.out.println("]");
+
+    }
+
+    public void delete(Node<E> current) {
+        current.data = current.next.data;
+        current.next = current.next.next;
+    }
+
     public Pair<SLL<E>, SLL<E>> splitAt(int n) {
         SLL<E> li = clone();
         li.drop(n);
@@ -216,12 +317,17 @@ public class SLL<E> {
         SLL<Pair<E, E>> result = new SLL<>();
         Node<E> current1 = head;
         Node<E> current2 = l2.head;
-        // Node<E> dummy = last;
-        // Node<E> current = head;
+        Node<Pair<E, E>> last = new Node<>(null);
+        Node<Pair<E, E>> dummy = last;
 
         while (current1 != null && current2 != null) {
-            result.addLast(new Pair<E, E>(current1.data, current2.data));
+            last.next = new Node<>(new Pair<>(current1.data, current2.data));
+            last = last.next;
+            current1 = current1.next;
+            current2 = current2.next;
         }
+
+        result.head = dummy.next;
         return result;
     }
 
@@ -241,4 +347,64 @@ public class SLL<E> {
         return s.toString();
     }
 
+    public static SLL<Integer> add(SLL<Integer> sll1, SLL<Integer> sll2) {
+
+        Node<Integer> current1 = sll1.head;
+        Node<Integer> current2 = sll2.head;
+
+        Node<Integer> last = new Node<>(null);
+        Node<Integer> dummy = last;
+
+        int carry = 0;
+
+        while (current1 != null && current2 != null) {
+            int sum = current1.data + current2.data + carry;
+            if (sum < 10) {
+                last.next = new Node<>(sum);
+                carry = 0;
+            } else {
+                carry = 1;
+                last.next = new Node<>(sum - 10);
+            }
+
+            last = last.next;
+
+            current1 = current1.next;
+            current2 = current2.next;
+        }
+
+        last.next = current1;
+        last.next = current2;
+
+        SLL<Integer> result = new SLL<>();
+        result.head = dummy.next;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        // SLL<Integer> s1 = new SLL<>();
+        // SLL<Integer> s2 = new SLL<>();
+        // for (int i = 6; i < 9; i++) {
+        // s1.addFirst(i);
+        // s2.addFirst(i);
+        // }
+        // for (int i = 5; i < 7; i++) {
+        // s2.addFirst(i);
+        // }
+        // System.out.println(s1);
+        // System.out.println(s2);
+        // System.out.println(SLL.add(s1, s2));
+        SLL<Integer> sll = new SLL<>();
+        for (int i = 0; i < 12; i++) {
+            sll.addFirst(i);
+        }
+        System.out.println(sll);
+        sll.printReverse();
+        // // System.out.println(sll);
+        // Node<Integer> node = sll.getNode(2);
+        // System.out.println(node.data);
+        // sll.delete(node);
+        // System.out.println(sll);
+
+    }
 }
